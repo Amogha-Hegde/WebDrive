d04cb898a81c56d8b0525f23364d072846d27a84
Amogha Hegde <amogha.hegde@hotmail.com>
2020-07-20 19:53:11 +0530

    Update settings.py

--- web_drive/settings.py
+++ web_drive/settings.py
@@ -81,13 +81,19 @@ WSGI_APPLICATION = 'web_drive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'webdrive',
+#         'USER':'postgres',
+#         'PASSWORD':'123456789',
+#         'HOST':'localhost',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'webdrive',
-        'USER':'postgres',
-        'PASSWORD':'123456789',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
